d4b7c690a2d4d012f4b6a8e2aa4018585fe2b2fa
123tanyapal <103447672+123tanyapal@users.noreply.github.com>
2023-02-08 12:46:47 +0530

    Creating email feature for form using react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import './App.css';
+
+import  { Register } from "./Register";
+
+function App() {
+   
+   
+
+  return (
+    <div className="App">
+      {
+         <Register/>
+      }
+      
+
+    </div>
+  );
+    }
+
+export default App;
